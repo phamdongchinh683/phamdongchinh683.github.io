0645a261802febb9ed01e01e4c8b34ecd38e1daf
phamdongchinh683 <phamdongchinh123789@gmail.com>
2025-04-21 10:54:02 +0700

    update 21-4-2025

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4181708680"
+    serviceWorkerVersion: "4099755120"
   }
 });
